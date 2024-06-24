9bdbdfd4d7294d789afdec0f9c1a1657fff40624
AndreLuisPLuz <andreluispluz@gmail.com>
2024-06-24 13:57:43 -0300

    feat: password and authentication services added.

--- second-stage/backend/src/main/java/com/trevis/startup/example/services/AuthService.java
+++ second-stage/backend/src/main/java/com/trevis/startup/example/services/AuthService.java
@@ -1,7 +1,25 @@
 package com.trevis.startup.example.services;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.trevis.startup.example.dto.AuthToken;
+import com.trevis.startup.example.exceptions.BadHashConfigurationException;
+import com.trevis.startup.example.exceptions.ClaimsConversionException;
+import com.trevis.startup.example.exceptions.InvalidAuthAttempt;
+import com.trevis.startup.example.exceptions.NoSuchEntityException;
 
 public interface AuthService {
-    public AuthToken login(String username, String password);
+    public AuthToken login(String username, String password)
+        throws
+            NoSuchAlgorithmException,
+            ClaimsConversionException,
+            NoSuchEntityException,
+            BadHashConfigurationException;
+    
+    public DecodedJWT decode(String token, Map<String, String> claimsList)
+        throws
+            NoSuchAlgorithmException,
+            InvalidAuthAttempt;
 }
