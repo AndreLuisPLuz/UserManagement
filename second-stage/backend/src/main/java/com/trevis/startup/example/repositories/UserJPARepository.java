a0eae31bf278b8ebd27ae6d6d4fac4eff9f44a58
AndreLuisPLuz <andreluispluz@gmail.com>
2024-06-24 14:14:52 -0300

    chore: removed unused imports, solved issues within mock services

--- second-stage/backend/src/main/java/com/trevis/startup/example/repositories/UserJPARepository.java
+++ second-stage/backend/src/main/java/com/trevis/startup/example/repositories/UserJPARepository.java
@@ -1,7 +1,6 @@
 package com.trevis.startup.example.repositories;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +8,4 @@ import com.trevis.startup.example.model.User;
 
 public interface UserJPARepository extends JpaRepository<User, Long>{
     List<User> findByUsernameContaining(String search);
-    Optional<User> findById(Long id);
 }
