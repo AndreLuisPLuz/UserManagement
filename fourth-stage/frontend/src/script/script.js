3319ef6c650ee04cb071abd52e86878e7aa39fa7
AndreLuisPLuz <andreluispluz@gmail.com>
2024-07-08 13:11:06 -0300

    chore: front-end added to fourth stage

---
+++ fourth-stage/frontend/src/script/script.js
@@ -0,0 +1,11 @@
+fetch("http://localhost:8080/test")
+    .then(response => response.text())
+    .then(text => {
+        let value = document.getElementsByClassName("message")[0]
+        value.innerHTML = text
+    })
+
+
+function redirectToHome() {
+    window.location.href = "../index.html";
+}
